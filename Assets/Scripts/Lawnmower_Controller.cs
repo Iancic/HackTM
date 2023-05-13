c9bce4baeb3eebd79c9c45ff39bd81220a4e4236
Iancic <davidiancic05@gmail.com>
2023-05-13 07:51:48 +0300

    Implementare Script Masina De Tuns
    Am implementat un script care controleaza masina si interactioneaza cu obiectele din jur. Mai multe explicatii in comentarii. Am implementat si UI care afiseaza numarul de bani pe care jucatorul ii detine.

--- Assets/Scripts/Lawnmower_Controller.cs
+++ Assets/Scripts/Lawnmower_Controller.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Lawnmower_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float moveSpeed = 0.01f;
+    private float multiSpeed = 0.0001f;
+    private float rotSpeed = 100f;
+    private float multirot = 1f;
+
+    private int money;
+
+    public TMP_Text money_text;
+
+    private void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        if (horizontalInput != 0)
+        {
+            RotateObject(horizontalInput);
+        }
+        MoveForward();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider collision)
     {
-        
+        if (collision.gameObject.tag == "Grass")
+        {
+            Destroy(collision.gameObject);
+            money += 10;
+            money_text.SetText("$" + money.ToString());
+        }
+        if (collision.gameObject.tag == "Obstacles")
+        {
+            Destroy(collision.gameObject);
+            money += 10;
+            money_text.SetText("$" + money.ToString());
+        }
+    }
+
+        private void MoveForward()
+    {
+        Vector3 movement = transform.forward * moveSpeed;
+        transform.position += movement;
+    }
+
+    private void RotateObject(float horizontalInput)
+    {
+        float rotationAmount = horizontalInput * rotSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.up, rotationAmount);
     }
 }
